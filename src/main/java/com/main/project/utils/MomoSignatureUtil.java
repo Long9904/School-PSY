fdc4923449d9679c3179b176d5c91a41c02a7e98
Long9904 <longvu09092004@gmail.com>
2025-04-12 15:59:38 +0700

    update util

--- src/main/java/com/main/project/utils/MomoSignatureUtil.java
+++ src/main/java/com/main/project/utils/MomoSignatureUtil.java
@@ -4,13 +4,14 @@ import org.apache.commons.codec.binary.Hex;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets; // Import lớp này
 
 public class MomoSignatureUtil {
     public static String signSHA256(String rawData, String secretKey) throws Exception {
-        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256"); // Sử dụng UTF-8
         Mac mac = Mac.getInstance("HmacSHA256");
         mac.init(secretKeySpec);
-        byte[] result = mac.doFinal(rawData.getBytes());
+        byte[] result = mac.doFinal(rawData.getBytes(StandardCharsets.UTF_8)); // Sử dụng UTF-8
         return Hex.encodeHexString(result);
     }
 }
